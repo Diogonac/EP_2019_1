dd6a3f70666edf30ff87f9e368568dc817e6ab38
Diogo Cintra <diogonac@al.insper.edu.br>
2019-04-18 22:44:25 -0300

    alteracao

--- ep1.py
+++ ep1.py
@@ -12,15 +12,15 @@ def carregar_cenarios():
             "opcoes": {
                 "elevador": "passar pela catraca e ir ao elevador A",
                 "escada": "passar pela catraca e ir para a escada da casa do pão de queijo",
-                "Saida": "Você vai para fora do prédio respirar um pouco"
+                "saida": "Você vai para fora do prédio respirar um pouco"
             }
         },
         "andar da sua sala": {
             "titulo": "Andar do desespero",
             "descricao": "Voce chegou ao andar da sala da sua prova",
             "opcoes": {
-                "inicio": "Desistir do vestibular e ir embora",
-                "sentar": "Achar a carteira com seu nome e sentar"
+                "fugir": "Ixiiii... não estou bem vou dar o vazante",
+                "sentar": "Achar sua sala e sua carteira com seu nome e sentar"
             }
         },
         "prova": {
@@ -34,28 +34,56 @@ def carregar_cenarios():
     nome_cenario_atual = "inicio"
     return cenarios, nome_cenario_atual
 
-def carregar_perguntas():
-    perguntas = {"pergunta1":"Você precisa soltar aquele barroso, mas gosta de extrema privacidade nessas horas. Em qual banheiro você vai?",
-                 "opcoes":{
+perguntas = {"pergunta1":"Você precisa soltar aquele barroso, mas gosta de extrema privacidade nessas horas. Em qual banheiro você vai?",
+                 "opcoes1":{
                          "segundo andar",
                          "decimo primeiro andar"
                          },
                  "pergunta2": "Acabou aquela aula infinita de GDE e você está morrendo de fome. Qual o horario ideal para ir almoçar?",
-                 "opcoes":{"12:00",
+                 "opcoes2":{"12:00",
                            "13:30"
+                         },
+                 "pergunta3": "Chegaram as semanas de provas, você fica na duivida café ou energetico. Como consegir café de graça?",
+                 "opcoes3":{"puxar o saco de alguém do multi insper",
+                           "marcar reunião com o marcos lisboa e tomar café na sala dele"
+                           },
+                 "pergunta4": "Você foi no momento insper a algum tempo e se perguntou: Quem é esse cara que fica indo de um lado pro outro?",
+                 "opcoes4":{"presidente do insper",
+                           "professor de engenharia"
+                         },
+                 "pergunta5":"Se você tivesse a opcao de ter comecado o ep1 semanas antes, começaria?",
+                 "opcoes5":{"sim",
+                           "não"
                          }
-            
-            
-            
-            
-            
-            
-            
-            
-            
-            
-            
             }
+                
+def resolvendo_prova():
+    acertos = 0
+    erros = 0
+    
+    print()
+    print("Está na hora de começar!")
+    
+    print("Pergunta 1")
+    print(perguntas["pergunta1"])
+    print('-'*len(perguntas["pergunta1"]))
+    print()
+    for e in perguntas['opcoes1']:
+        print(e)
+    print()
+    escolha = input('O que você quer fazer?')
+    
+    if escolha == 'decimo primeiro andar':
+        acertos+=1
+    else:
+        erros=+1
+
+
+        
+
+    
+    
+                
 def main():
     print("Na hora do sufoco!")
     print("------------------")
@@ -71,6 +99,10 @@ def main():
     cenarios, nome_cenario_atual = carregar_cenarios()
 
     game_over = False
+    
+
+    tempo = 0
+    
     while not game_over:
         cenario_atual = cenarios[nome_cenario_atual]
 
@@ -80,18 +112,125 @@ def main():
         print(cenario_atual["descricao"])
         
         opcoes = cenario_atual['opcoes']
+        
         if len(opcoes) == 0:
             print("Acabaram-se suas opções! Mwo mwo mwooooo...")
             game_over = True
         else:
-
-
             print()
             for k,v in cenario_atual['opcoes'].items():
                 print('{0}: {1}'.format(k,v))
             print()
 
-            escolha = input('O que voce quer fazer?')
+            escolha = input('O que você quer fazer?')
+#Opção saida
+            if escolha == 'saida':
+                print("Escolha errada meu caro, tenha mais coragem!")
+                game_over = True
+#Opção elevador                
+            if escolha == 'elevador':
+                print("Aguarde o elevador chegar")
+                while tempo<1e8:
+                    tempo+=1
+                print("Plinn plinn... Porta abrindo")
+                print("Neste momento você entrou no elevador e  chegou no seu andar")
+                
+                cenario_atual = cenarios["andar da sua sala"]
+                print(cenario_atual["titulo"])
+                print('-'*len(cenario_atual["titulo"]))
+                print(cenario_atual["descricao"])
+                
+                print()
+                for a,b in cenario_atual['opcoes'].items():
+                    print('{0}: {1}'.format(a,b))
+                    print()
+            
+                escolha = input("O que você quer fazer?")
+                if escolha == 'fugir':
+                    game_over = True
+                    print("Seja mais forte da próxima vez")
+                    
+                if escolha == 'sentar':          
+                    cenario_atual = cenarios["prova"]
+                    print(cenario_atual["titulo"])
+                    print('-'*len(cenario_atual["titulo"]))
+                    print(cenario_atual["descricao"])
+                    print()
+                    for c,d in cenario_atual['opcoes'].items():
+                        print('{0}: {1}'.format(c,d))
+                        print()  
+                    escolha = input("O que você quer fazer?")
+                    
+                    if escolha == 'vamo que vamo':
+                        resolvendo_prova()
+                    if escolha == 'vish vou dar um migue':
+                        game_over = True
+                        
+#Opção escada
+            if escolha == 'escada':
+                print("Neste momento você está subindo a escada")
+                print("Aguarde...")
+                while tempo<1e7:
+                    tempo+=1
+                cenario_atual = cenarios["andar da sua sala"]
+                print(cenario_atual["titulo"])
+                print('-'*len(cenario_atual["titulo"]))
+                print(cenario_atual["descricao"])
+                
+                print()
+                for a,b in cenario_atual['opcoes'].items():
+                    print('{0}: {1}'.format(a,b))
+                    print()
+            
+                escolha = input("O que você quer fazer?")
+                if escolha == 'fugir':
+                    game_over = True
+                    print("Seja mais forte da próxima vez")
+                    
+                if escolha == 'sentar':          
+                    cenario_atual = cenarios["prova"]
+                    print(cenario_atual["titulo"])
+                    print('-'*len(cenario_atual["titulo"]))
+                    print(cenario_atual["descricao"])
+                    print()
+                    for c,d in cenario_atual['opcoes'].items():
+                        print('{0}: {1}'.format(c,d))
+                        print()  
+                    escolha = input("O que você quer fazer?")
+                    
+                    if escolha == 'vamo que vamo':
+                        #resolvendo_prova()
+                        print("chama a função")
+                    if escolha == 'vish vou dar um migue':
+                        game_over = True
+                
+                
+                
+            
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
             if escolha in opcoes:
@@ -100,7 +239,7 @@ def main():
                 print("Sua indecisão foi sua ruína!")
                 game_over = True
 
-    print("Você morreu!")
+    print("Game Over!")
 
 
 # Programa principal.
